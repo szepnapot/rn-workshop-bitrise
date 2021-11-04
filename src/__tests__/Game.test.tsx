6a89c988adb3d0dc08856416dc6084531ae7b678
Fodor Dániel <daniel.fodor@benestudio.co>
2021-11-04 19:55:40 +0100

    fix snapshot testing

--- src/__tests__/Game.test.tsx
+++ src/__tests__/Game.test.tsx
@@ -4,6 +4,14 @@ import { Game } from '../screens';
 import { IProps } from '../screens/Game';
 import { IQuiz } from '../interfaces';
 
+jest.mock('../utils/helpers', () => {
+  const og = jest.requireActual('../utils/helpers');
+  return {
+    ...og,
+    shuffle: jest.fn((arr) => arr),
+  };
+});
+
 const props: IProps = {
   currentIndex: 0,
   isLoading: false,
